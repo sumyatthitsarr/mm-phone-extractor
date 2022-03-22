a14b6b926286ee5030a43800755524d6b98b7378
Sumyatthitsarr <sumyatthitsarr@gmail.com>
2022-03-22 13:21:26 +0630

    adding burmese integer extraction

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from data_extraction import get_phone_data
 
 # test_str = 'My number is +959982453277 and 09982453279'
 test_str = 'My number is +959982453277'
+test_str = 'ဖုန်းလေးက ၀၉၇၅၃၆၁၂၈၄၂' 
 # test_str = 'My number is 09421145678'
 # test_str = 'My number is 09421145678'
 
